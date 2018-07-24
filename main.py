b4ad33fd1c6f4ca1ca932b449f9b576639a36a09
Eagle19243 <eagle19243@gmail.com>
2018-07-24 21:16:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('SPN2 with Google Sheet-6669bdd49f71.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("ISKME Press Mention URLs ").sheet1
+
+list_of_hashes = sheet.get_all_records()
+print(list_of_hashes)
